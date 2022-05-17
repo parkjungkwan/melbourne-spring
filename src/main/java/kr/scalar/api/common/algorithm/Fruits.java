6c0f8759516db9111369619b095d2613e34525b9
parkjungkwan <pakjkwan@gmail.com>
2022-05-17 17:01:19 +0900

    17

---
+++ src/main/java/kr/scalar/api/common/algorithm/Fruits.java
@@ -0,0 +1,16 @@
+package kr.scalar.api.common.algorithm;
+
+/**
+ * packageName: kr.scalar.api.common.algorithm
+ * fileName   : Frutes
+ * author     : parkjungkwan
+ * date       : 2022-05-17
+ * desc       :
+ * ================================
+ * DATE        AUTHOR        NOTE
+ * ================================
+ * 2022-05-17   parkjungkwan  최초 생성
+ */
+public class Fruits{
+
+}
--- src/main/java/kr/scalar/api/common/algorithm/PrimeNumber.java
+++ src/main/java/kr/scalar/api/common/algorithm/PrimeNumber.java
@@ -1,5 +1,11 @@
 package kr.scalar.api.common.algorithm;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 /**
@@ -14,7 +20,16 @@ import java.util.List;
  * 2022-05-17   parkjungkwan  최초 생성
  */
 public class PrimeNumber {
-    int start, end;
-    List<Integer> primes;
+    @Builder @Getter @AllArgsConstructor @NoArgsConstructor
+    static class Solution{
+        int start, end;
+        List<Integer> primes;
 
+    }
+    @FunctionalInterface static interface SolutionService{ Solution solution(Solution solution);}
+
+    @Test
+    void solution(){
+
+    }
 }
