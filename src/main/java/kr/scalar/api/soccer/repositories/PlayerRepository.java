255b36aa47c19f8e052d29d8900e5e9e00393432
parkjungkwan <pakjkwan@gmail.com>
2022-05-20 15:44:26 +0900

    15

--- src/main/java/kr/scalar/api/soccer/repositories/PlayerRepository.java
+++ src/main/java/kr/scalar/api/soccer/repositories/PlayerRepository.java
@@ -1,7 +1,11 @@
 package kr.scalar.api.soccer.repositories;
 import kr.scalar.api.soccer.domains.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * packageName   :   com.toomuchcoder.api.soccer.repositories
  * fileName      :   PlayerRepository
@@ -15,6 +19,30 @@ import org.springframework.stereotype.Repository;
  */
 interface PlayerCustomRepository{
     // 선수들의 키와 몸무게를 변경하시오
+    @Query(value="")
+    int update();
+
+    // 002. 플레이어의 포지션 종류를 나열하시오.단 중복은 제거하시오
+    @Query(value = "select distinct p.position from Player p")
+    List<String> findPositions();
+
+    // 004. 수원팀에서 골키퍼(GK)의 이름을 모두 출력하시오.
+    // 단 수원팀 ID는 K02 입니다.
+    @Query(value = "select p.playerName \n"
+    + "from Player p \n"
+    + "where p.teamId like :teamId and p.position like :teamId")
+    List<String> findTeamGKByTeamId();
+
+    // 005. 수원팀에서 성이 고씨이고 키가 170 이상인 선수를 출력하시오.
+    // 단 수원팀 ID는 K02 입니다.
+    @Query(value = "select p.playerName \n"
+    + "from Player p \n"
+    + "where p.teamId like :teamId \n"
+    + "and p.playerName like :familyName \n"
+    + "and p.height >= :height")
+    List<Player> findPlayers(String teamId, String familyName, String height);
+
+
 }
 
 @Repository
