ac213df67421ca97e152a43562027d04d5cd5681
parkjungkwan <pakjkwan@gmail.com>
2022-05-25 18:05:24 +0900

    18

--- src/main/java/kr/scalar/api/common/_sort/가장큰수.java
+++ src/main/java/kr/scalar/api/common/_sort/가장큰수.java
@@ -1,5 +1,10 @@
 package kr.scalar.api.common._sort;
 
+import kr.scalar.api.common._bruteForce.계산기;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
 /**
  * packageName: kr.scalar.api.common._sort
  * fileName   : 가장큰수
@@ -12,4 +17,21 @@ package kr.scalar.api.common._sort;
  * 2022-05-24   parkjungkwan  최초 생성
  */
 public class 가장큰수 {
+    @Data
+    @AllArgsConstructor
+    static class Solution{
+
+        @Override public String toString(){
+
+
+            return "";
+        }
+    }
+    @FunctionalInterface interface SolutionService {
+        Solution solution(Solution s);
+    }
+    @Test
+    void testSolution(){
+
+    }
 }
